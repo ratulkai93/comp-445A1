c6066a34c58d8fdf816d43b015d1e31fbcf28323
cmy2415 <68174349+cmy2415@users.noreply.github.com>
2022-02-14 16:35:40 -0500

    Update httpGP.py
    Finished display() method for both regular and -v option

--- httpGP.py
+++ httpGP.py
@@ -3,55 +3,72 @@ import argparse
 import sys
 import json
 from urllib.parse import urlsplit, parse_qs
-#todo: parse the url from cmd, shove into the method to send request
+
+
+# todo: parse the url from cmd, shove into the method to send request
 
 def run_httpcClient():
-    line= input('type your request: ')
-    parsedLine= line.split(" ")
-    if(parsedLine[2]=="-v"):
-        parsedUrl= "".join(map(str,parsedLine[3]))#checks for verbose  and converts the following list item to string
+
+    line = input('type your request: ')
+    parsedLine = line.split(" ")
+
+    needVerbose = False
+    if (parsedLine[2] == "-v"):
+        parsedUrl = "".join(
+            map(str, parsedLine[3]))  # checks for verbose  and converts the following list item to string
+        needVerbose = True
     else:
-        parsedUrl= "".join(map(str,parsedLine[2]))#converts the list item to string
-            
-    httpc_get(parsedUrl)
-        
-        
-#parses and sends get request w/ provided url (has additional info for verbose)
-def httpc_get(url):
+        parsedUrl = "".join(map(str, parsedLine[2]))  # converts the list item to string
+
+    if (parsedLine[1] == "get"):
+        httpc_get(parsedUrl, needVerbose)
+    if (parsedLine[1] == "post"):
+        httpc_post(parsedUrl, needVerbose)
+
+
+# parses and sends get request w/ provided url (has additional info for verbose)
+def httpc_get(url, needVerbose):
     _, _, host, path = url.split('/', 3)
     addr = socket.getaddrinfo(host, 80)[0][-1]
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(addr)
     s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
-    while True:
-        data = s.recv(100)
-        if data:
-            print(str(data, 'utf8'), end='')
-        else:
-            break
+    display(s, needVerbose)
     s.close()
-    
-def httpc_post(url):
+
+
+def httpc_post(url, needVerbose):
     print('sending post')
     _, _, host, path = url.split('/', 3)
     addr = socket.getaddrinfo(host, 80)[0][-1]
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(addr)
-    #s.send(bytes('POST /%s HTTP/1.0\r\n Host: %s\r\nContent-Type: application/json\r\nContent-Length: 47\r\n\r\n{"capabilities": {}, "desiredCapabilities": {}}'  % (path, host), 'utf8'))
+    # s.send(bytes('POST /%s HTTP/1.0\r\n Host: %s\r\nContent-Type: application/json\r\nContent-Length: 47\r\n\r\n{"capabilities": {}, "desiredCapabilities": {}}'  % (path, host), 'utf8'))
     s.send(bytes('POST /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
+    display(s, needVerbose)
+    s.close()
+
+
+# display the data from socket, includes verbose info if needed
+def display(s, needVerbose):
+    strRecv = ""
     while True:
         data = s.recv(100)
         if data:
-            print(str(data, 'utf8'), end='')
+            strRecv = strRecv + (str(data, 'utf8'))
         else:
             break
-    s.close()
+    if needVerbose:
+        print(strRecv)  # Print w/ verbose info
+    else:
+        print(strRecv.split("\r\n\r", 1)[1])  # Print w/o verbose info
 
-run_httpcClient() 
-#paste this on the terminal when running the file---> httpc get http://httpbin.org/get?course=networking&assignment=1
 
-#to test GET request only   
-#httpc_get('http://httpbin.org/get?course=networking&assignment=1')
+run_httpcClient()
+# paste this on the terminal when running the file---> httpc get http://httpbin.org/get?course=networking&assignment=1
 
-#to test POST request only ----> not functional atm!
-#httpc_post('http://httpbin.org/get?course=networking&assignment=1')
+# to test GET request only
+# httpc_get('http://httpbin.org/get?course=networking&assignment=1')
+
+# to test POST request only ----> not functional atm!
+# httpc_post('http://httpbin.org/get?course=networking&assignment=1')

7455f0b34684a17e4c672edb20f03d70f8b7dc60
Rahimuz Zaman <rahimuz_z@yahoo.ca>
2022-04-11 23:07:00 -0400

    wip

--- UDPctrlr.py
+++ UDPctrlr.py
@@ -1,8 +1,10 @@
 from ipaddress import ip_address
-import logging
+from operator import truediv
 import time
 import socket
+from tkinter import W
 from Packet import *
+from Window import *
 import sys
 
 #helper constants    
@@ -10,6 +12,7 @@ SERVER_IP="localhost"
 SERVER_PORT = 8007
 ROUTER_IP = "localhost"
 ROUTER_PORT = 3000
+ALIVE= 10
 
 class Packetbuilder:
     __destAddr=None
@@ -30,6 +33,23 @@ class UDPctrlr:
     def __init__(self):
         pass
     
+    def getPacket(self,tOut):
+        self.__conn.settimeout(tOut)
+        try:
+            pData, address= self.__conn.recvfrom(PACKET_SIZE)
+            pkt= Packet.from_bytes(pData)
+            print("Receved:"+ pkt + pkt.payload )
+            self.__routerAddr=address
+            
+            if self.__pcktBuilder is None:
+                self.__pcktBuilder=Packetbuilder(pkt.peer_ip_addr, pkt.peer_port)
+            
+            return pkt 
+        except socket.timeout:
+            print("Timeout bc of recvfrom msg..")
+            return None
+    
+    #client ---> server
     def connectToServer(self):
         #for 3-way handshake
         print("connecting to :"+SERVER_IP+ " : "+SERVER_PORT)
@@ -38,7 +58,112 @@ class UDPctrlr:
         self.__pcktBuilder = Packetbuilder(peer_ip, SERVER_PORT)
         self.__conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)#udp 
         try:
-            #syn from client to server
-            p1= self.__pcktBuilder.pcktBuild(PACKET_TYPE_SYN)#to fix
-            self.__conn.sendto(p1.to)
+            #syn sent from client
+            p1= self.__pcktBuilder.pcktBuild(PACKET_TYPE_SYN)
+            self.__conn.sendto(p1.to_bytes(),self.__routerAddr)
+            self.__conn.settimeout(ALIVE)
+            print("Waiting for server response...\n")
+            resp, sender= self.__conn.recvfrom(PACKET_SIZE)# expecting syn/ack from server
+            p1=Packet.from_bytes(resp)         
+            print("Server connection is ON!")
+        except socket.timeout:
+            print("Connection has timed out!")
+            self.__conn.close()
+            sys.exit(0)
             
+        if p1.packet_type==PACKET_TYPE_SYN_AK:
+            p1=self.__pcktBuilder.pcktBuild(PACKET_TYPE_AK) #sending AK to server
+            self.__conn.sendto(p1.to_bytes(), self.__routerAddr)
+            return True #Servers ready, no need to timeout
+        else:
+            print("Unexpected packet as follows: "+ p1) 
+            self.__conn.close()
+            sys.exit(0)    
+                   
+    #server--->client
+    def connectToClient(self):
+        self.__conn=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.__conn.bind('',SERVER_PORT)
+        print("Server is listening at "+ SERVER_IP+" : "+SERVER_PORT)
+        
+        pk= self.getPacket(ALIVE)
+        
+        if pk is None:
+            print("Connection timeout...")
+            return False
+        
+        if pk.packet_type == PACKET_TYPE_SYN: #if server already received SYN, send back ACK/SYN
+            pk.packet_type = PACKET_TYPE_SYN_AK
+            self.__conn.sendto(pk.to_bytes(), self.__routerAddr) #We can ignore the incoming ACK bc client wont have to deal with this case 
+            print("Client connection established.")
+            return True
+        
+        return False
+    
+    def sendMsg(self, msg):
+        wd= Window()
+        wd.createSenderWindow(msg)        
+        threading.Thread(target=self.senderListener, args=(wd,)).start()
+        while wd.hasPendingPacket():  # Not all packets have been sent
+            #if theres any window, get the packets that can be sent
+            for frame in wd.getFrames():
+                p = self.__pcktBuilder.pcktBuild(PACKET_TYPE_DATA, frame.seq_num, frame.payload)
+                self.__conn.sendto(p.to_bytes(), self.__routerAddr)
+                print("sending message: "+ p.payload)
+                frame.timer = time.time()
+                frame.send = True
+    
+    #Listening response from server 
+    def senderListener(self, window):        
+        while window.hasPendingPacket():
+            # Find sent packets that haven't been ACKed & check the timers
+            try:
+                self.__conn.settimeout(TIME_OUT)
+                response, sender = self.__conn.recvfrom(PACKET_SIZE)
+                p = Packet.from_bytes(response)
+                #logging.debug('[Transport] Received response: {}: {}'.format(p, p.payload.decode("utf-8")))
+                if p.packet_type == PACKET_TYPE_AK:
+                    window.updateWindow(p.seq_num)
+            except socket.timeout:
+                #logging.debug("[Transport] Timeout when wait ACK.")
+                for i in range(window.pointer, window.pointer + WINDOW_SIZE):
+                    if i >= len(window.frames):
+                        break
+                    f = window.frames[i]
+                    if f.send and not f.ACK:
+                        # reset send status, so it can be re-sent
+                        f.send = False
+
+        #logging.debug('[Transport] Listener reaches the end!')
+    
+    def receiveMessage(self):
+        window = Window()
+        window.createReceiverWindow()
+        while not window.finished():
+            # TODO if None, raise error
+            p = self.getPacket(TIME_OUT_FOR_RECEIVE)
+            if p is None:
+                #logging.debug("[Transport] No message received in timeout time")
+                return None
+            # discard possible packet from handshake
+            if p.packet_type == PACKET_TYPE_AK and p.seq_num == 0:
+                continue
+            window.process(p)
+            # send ACK
+            p.packet_type = PACKET_TYPE_AK
+            self.__conn.sendto(p.to_bytes(), self.__routerAddr)
+
+        data = self.retrieveData(window)
+        return data
+    
+    # return data (bytes)
+    def retrieveData(self, window):
+        data = b''
+        for f in window.frames:
+            data = data + f.payload
+        return data   
+     
+    def dis_connect(self):
+        #Disconnecting: FIN, ACK, FIN, ACK
+        logging.info("Disconnecting.")
+        self.__conn.close()    
---
+++ Window.py
@@ -0,0 +1,98 @@
+import time
+import threading
+import math
+from Packet import *
+
+class Frame:
+    def __init__(self, seq_num, payload=None, is_last=False):
+        # sequence number
+        self.seq_num = seq_num
+        # data
+        self.payload = payload
+        self.is_last = is_last
+        self.send = False
+        self.ACK = False
+
+class Window:
+    def __init__(self):
+        # where WINDOW starts from
+        self.pointer = 0
+        # data
+        self.frames = []
+        self.numberOfFrames = 0
+        self.fini = False
+
+    def createSenderWindow(self, message):
+        # number of packets
+        self.numberOfFrames = math.ceil(len(message)/PAYLOAD_SIZE) 
+        # init all packets
+        for i in range(0, self.numberOfFrames):
+            if i == self.numberOfFrames - 1:
+                self.frames.append(Frame(i + 1, message[i * PAYLOAD_SIZE:], True))
+            else:
+                self.frames.append(Frame(i + 1, message[i * PAYLOAD_SIZE:(i + 1) * PAYLOAD_SIZE]))
+
+    def createReceiverWindow(self):
+        pass
+
+    def getFrames(self):
+        """
+        Get all sendable packets in WINDOW
+        """
+        frameList = []
+        for i in range(self.pointer, self.pointer + WINDOW_SIZE):
+            if i >= len(self.frames):
+                break
+            f = self.frames[i]
+            if not f.send:
+                frameList.append(f)
+        return frameList
+
+    def updateWindow(self, seq_num):
+        """
+        When received an ACK, update WINDOW, slide WINDOW if necessary
+        """
+        self.frames[seq_num - 1].ACK = True
+        offset = 0
+        for i in range(self.pointer, self.pointer + WINDOW_SIZE):
+            if i >= len(self.frames):
+                break
+            if self.frames[i].ACK:
+                offset += 1
+            else:
+                break
+        self.pointer += offset
+
+    def hasPendingPacket(self):
+        """
+        Check whether all packets have been ACKed
+        """
+        for i in range(0, self.numberOfFrames):
+            if not self.frames[i].ACK:
+                return True
+        return False
+
+    def finished(self):
+        # check payload ###total number###
+        # if is last one, update fini
+        if self.frames:
+            f = self.frames[-1]
+            if f.is_last:
+                if self.pointer == len(self.frames):
+                    self.fini = True
+
+        return self.fini
+
+    def process(self, p):
+        # Is it in WINDOW range?
+        index = p.seq_num - 1
+        if self.pointer <= index < self.pointer + WINDOW_SIZE:
+            # check whether already received
+            while index >= len(self.frames):
+                self.frames.append(None)
+            if self.frames[index] is None:
+                self.frames[index] = Frame(index, p.payload, True)
+                self.updateWindow(index)
+        else:
+            # discard this packet
+            pass
